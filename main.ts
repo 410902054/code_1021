14fdec70394a7082821ea959067d217f0094d601
410902054 <71751283+410902054@users.noreply.github.com>
2020-10-21 15:37:23 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 for (let i = 0; i <= 4; i++) {
     for (let j = 0; j <= 4; j++) {
-        if (i % 2 == j % 2) {
+        if (i % 2 != j % 2) {
             led.plot(i, j)
             basic.pause(100)
         }
